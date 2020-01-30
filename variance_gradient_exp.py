fix true grad experiment, add gradient variance experiment

variance_gradient_exp.py
@@ -1,5 +1,6 @@
 import copy
 import glob
+import itertools
 import os
 import pickle
 import time
@@ -37,11 +38,11 @@ def main():
     ppo_epoch = 10
     use_proper_time_limits = True
     num_steps_list = [200,1000,2000,10000,20000,100000,200000,1000000]
-    # num_baseline_steps = 100
+    num_baseline_steps = 2500
     save_interval = 100
     log_interval = 10
-    num_processes = 10
-    num_env_steps = 100000
+    num_processes = 20
+    num_env_steps = 10000000
     use_linear_lr_decay = True
     clip_param = 0.2
     lr = 1e-4
@@ -69,13 +70,19 @@ def main():
     device = "cpu"
 
     envs = make_vec_envs(env_name, seed, num_processes,
-                         gamma, log_dir, device, allow_early_resets=True)
+                         gamma, log_dir, device, True)
 
     actor_critic = Policy(
         envs.observation_space.shape,
         envs.action_space,
         base_kwargs={'recurrent': False})
     actor_critic.to(device)
+
+    actor_critic_oracle = Policy(
+        envs.observation_space.shape,
+        envs.action_space,
+        base_kwargs={'recurrent': False})
+    actor_critic_oracle.to(device)
 
     if mode == 'PPO':
         agent = algo.PPO(
@@ -111,8 +118,13 @@ def main():
     else:
         raise Exception('Algorithm not specified correctly')
 
-    # with open('./store/rollout-100k-flat.pkl', 'rb') as rollout_file:
-    #     rollouts_5m = pickle.load(rollout_file)
+    # rollouts = RolloutStorage(num_steps, num_processes,
+    #                           envs.observation_space.shape, envs.action_space,
+    #                           actor_critic.recurrent_hidden_state_size)
+
+    # obs = envs.reset()
+    # rollouts.obs[0].copy_(obs)
+    # rollouts.to(device)
 
     episode_rewards = deque(maxlen=10)
 
@@ -120,85 +132,31 @@ def main():
     # num_updates = int(
     #     num_env_steps) // num_steps // num_processes
 
-    # num_updates = 1
-
-    j = 0
-
-    cos_sims = []
-
-    # with torch.no_grad():
-    #     next_value_5m = actor_critic.get_value(
-    #         rollouts_5m.obs[-1], rollouts_5m.recurrent_hidden_states[-1],
-    #         rollouts_5m.masks[-1]
-    #     ).detach()
-
-    # rollouts_5m.compute_returns(next_value_5m, False, gamma,
-    #                             gae_lambda, use_proper_time_limits)
+    num_updates = 1
+    ave_cos_sims = []
 
     num_steps_list_norm = torch.tensor(num_steps_list) / num_processes
-
-    baseline_steps = 5e6
-    env_steps_per_worker = int(baseline_steps / num_processes)
-
-    rollouts_5m = RolloutStorage(env_steps_per_worker, num_processes,
-                            envs.observation_space.shape, envs.action_space,
-                            actor_critic.recurrent_hidden_state_size)
-    
-    obs = envs.reset()
-    rollouts_5m.obs[0].copy_(obs)
-    rollouts_5m.to(device)
-
-    actor_critic = Policy(
-        envs.observation_space.shape,
-        envs.action_space,
-        base_kwargs={'recurrent': False})
-    actor_critic.to(device)
-
-    agent = algo.PPO(
-        actor_critic,
-        clip_param,
-        ppo_epoch=ppo_epoch,
-        num_mini_batch=num_mini_batch,
-        value_loss_coef=value_loss_coef,
-        entropy_coef=entropy_coef,
-        lr=lr,
-        eps=eps,
-        max_grad_norm=max_grad_norm)
-
-    for step in range(env_steps_per_worker):
-        # Sample actions
-        with torch.no_grad():
-            value, action, action_log_prob, recurrent_hidden_states = actor_critic.act(
-                rollouts_5m.obs[step], rollouts_5m.recurrent_hidden_states[step],
-                rollouts_5m.masks[step])
-
-        # Obser reward and next obs
-        obs, reward, done, infos = envs.step(action)
-
-        for info in infos:
-            if 'episode' in info.keys():
-                episode_rewards.append(info['episode']['r'])
-
-        # If done then clean the history of observations.
-        masks = torch.FloatTensor(
-            [[0.0] if done_ else [1.0] for done_ in done])
-        bad_masks = torch.FloatTensor(
-            [[0.0] if 'bad_transition' in info.keys() else [1.0]
-                for info in infos])
-        rollouts_5m.insert(obs, recurrent_hidden_states, action,
-                        action_log_prob, value, reward, masks, bad_masks)
-    
-    with torch.no_grad():
-        next_value = actor_critic.get_value(
-            rollouts_5m.obs[-1], rollouts_5m.recurrent_hidden_states[-1],
-            rollouts_5m.masks[-1]).detach()
-
-    rollouts_5m.compute_returns(next_value, use_gae, gamma,
-                                gae_lambda, use_proper_time_limits)
 
     for num_steps in num_steps_list_norm:
 
         num_steps = num_steps.item()
+
+        actor_critic = Policy(
+            envs.observation_space.shape,
+            envs.action_space,
+            base_kwargs={'recurrent': False})
+        actor_critic.to(device)
+
+        agent = algo.PPO(
+            actor_critic,
+            clip_param,
+            ppo_epoch=ppo_epoch,
+            num_mini_batch=num_mini_batch,
+            value_loss_coef=value_loss_coef,
+            entropy_coef=entropy_coef,
+            lr=lr,
+            eps=eps,
+            max_grad_norm=max_grad_norm)
 
         rollouts = RolloutStorage(num_steps, num_processes,
                             envs.observation_space.shape, envs.action_space,
@@ -208,54 +166,54 @@ def main():
         rollouts.obs[0].copy_(obs)
         rollouts.to(device)
 
-        # if use_linear_lr_decay:
-        #     # decrease learning rate linearly
-        #     utils.update_linear_schedule(
-        #         agent.optimizer, 0, 1, lr)
+        grad_estimates = []
+        for i in range(10):
+            for step in range(num_steps):
+                # Sample actions
+                with torch.no_grad():
+                    value, action, action_log_prob, recurrent_hidden_states = actor_critic.act(
+                        rollouts.obs[step], rollouts.recurrent_hidden_states[step],
+                        rollouts.masks[step])
 
-        for step in range(num_steps):
-            # Sample actions
+                # Obser reward and next obs
+                obs, reward, done, infos = envs.step(action)
+
+                for info in infos:
+                    if 'episode' in info.keys():
+                        episode_rewards.append(info['episode']['r'])
+
+                # If done then clean the history of observations.
+                masks = torch.FloatTensor(
+                    [[0.0] if done_ else [1.0] for done_ in done])
+                bad_masks = torch.FloatTensor(
+                    [[0.0] if 'bad_transition' in info.keys() else [1.0]
+                    for info in infos])
+                rollouts.insert(obs, recurrent_hidden_states, action,
+                                action_log_prob, value, reward, masks, bad_masks)
+
             with torch.no_grad():
-                value, action, action_log_prob, recurrent_hidden_states = actor_critic.act(
-                    rollouts.obs[step], rollouts.recurrent_hidden_states[step],
-                    rollouts.masks[step])
+                next_value = actor_critic.get_value(
+                    rollouts.obs[-1], rollouts.recurrent_hidden_states[-1],
+                    rollouts.masks[-1]).detach()
 
-            # Obser reward and next obs
-            obs, reward, done, infos = envs.step(action)
+            rollouts.compute_returns(next_value, use_gae, gamma,
+                                    gae_lambda, use_proper_time_limits)
+            
+            grad_estimates.append(agent.get_grad_vector(rollouts))
 
-            for info in infos:
-                if 'episode' in info.keys():
-                    episode_rewards.append(info['episode']['r'])
+            # cosine_sim = torch.nn.functional.cosine_similarity(grad_full, grad_est, dim=0)
+            # print("Cosine similarity:", cosine_sim.item())
 
-            # If done then clean the history of observations.
-            masks = torch.FloatTensor(
-                [[0.0] if done_ else [1.0] for done_ in done])
-            bad_masks = torch.FloatTensor(
-                [[0.0] if 'bad_transition' in info.keys() else [1.0]
-                 for info in infos])
-            rollouts.insert(obs, recurrent_hidden_states, action,
-                            action_log_prob, value, reward, masks, bad_masks)
+            if i == 0:
+                value_loss, action_loss, dist_entropy = agent.update(rollouts)
 
-        with torch.no_grad():
-            next_value = actor_critic.get_value(
-                rollouts.obs[-1], rollouts.recurrent_hidden_states[-1],
-                rollouts.masks[-1]).detach()
+            rollouts.after_update()
 
-        rollouts.compute_returns(next_value, use_gae, gamma,
-                                 gae_lambda, use_proper_time_limits)
-        
-        grad_est = agent.get_grad_vector(rollouts)
-        agent.optimizer.zero_grad()
-        grad_full = agent.get_grad_vector(rollouts_5m)
-
-
-        cosine_sim = torch.nn.functional.cosine_similarity(grad_full, grad_est, dim=0)
-        print("Cosine similarity:", cosine_sim.item())
-        cos_sims.append(cosine_sim.item())
-
-        # value_loss, action_loss, dist_entropy = agent.update(rollouts)
-
-        # rollouts.after_update()
+        cos_sims = []
+        for a, b in itertools.combinations(grad_estimates, 2):
+            cos_sims.append(torch.nn.functional.cosine_similarity(a, b, dim=0))
+        cos_sims = torch.tensor(cos_sims)
+        ave_cos_sims.append(cos_sims.mean().item())
 
         # save for every interval-th episode or for the last epoch
         # if (j % save_interval == 0
@@ -289,9 +247,8 @@ def main():
         #     ob_rms = utils.get_vec_normalize(envs).ob_rms
         #     evaluate(actor_critic, ob_rms, args.env_name, args.seed,
         #              args.num_processes, eval_log_dir, device)
-
-    df = pd.DataFrame({'batch_sizes': num_steps_list, 'cos-sim': cos_sims})
-    df.to_csv('./store/true-grad-cos-sim.csv')
+    df = pd.DataFrame({'batch_sizes': num_steps_list, 'ave-cos-sim': ave_cos_sims})
+    df.to_csv('./store/variance-grad-cos-sim.csv')
 
 if __name__ == "__main__":
     main()

variance_value_func_exp.py
@@ -1,9 +1,12 @@
 import copy
 import glob
+import itertools
 import os
 import pickle
 import time
 from collections import deque
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
 import gym
 import numpy as np
@@ -33,12 +36,12 @@ def main():
     num_mini_batch = 32
     ppo_epoch = 10
     use_proper_time_limits = True
-    num_steps = 2000
-    # num_baseline_steps = 100
+    num_steps = 100
+    num_baseline_steps = 2500
     save_interval = 100
     log_interval = 10
-    num_processes = 1
-    num_env_steps = 5000000
+    num_processes = 20
+    num_env_steps = 10000000
     use_linear_lr_decay = True
     clip_param = 0.2
     lr = 1e-4
@@ -74,9 +77,26 @@ def main():
         base_kwargs={'recurrent': False})
     actor_critic.to(device)
 
+    actor_critic_oracle = Policy(
+        envs.observation_space.shape,
+        envs.action_space,
+        base_kwargs={'recurrent': False})
+    actor_critic_oracle.to(device)
+
     if mode == 'PPO':
         agent = algo.PPO(
             actor_critic,
+            clip_param,
+            ppo_epoch=ppo_epoch,
+            num_mini_batch=num_mini_batch,
+            value_loss_coef=value_loss_coef,
+            entropy_coef=entropy_coef,
+            lr=lr,
+            eps=eps,
+            max_grad_norm=max_grad_norm)
+        
+        agent_oracle = algo.PPO(
+            actor_critic_oracle,
             clip_param,
             ppo_epoch=ppo_epoch,
             num_mini_batch=num_mini_batch,
@@ -112,21 +132,18 @@ def main():
                               envs.observation_space.shape, envs.action_space,
                               actor_critic.recurrent_hidden_state_size)
 
-    rollouts_5m = RolloutStorage(num_env_steps, num_processes,
-                              envs.observation_space.shape, envs.action_space,
-                              actor_critic.recurrent_hidden_state_size)
-
     obs = envs.reset()
     rollouts.obs[0].copy_(obs)
     rollouts.to(device)
-    rollouts_5m.obs[0].copy_(obs)
-    rollouts_5m.to(device)
 
     episode_rewards = deque(maxlen=10)
 
     start = time.time()
     num_updates = int(
         num_env_steps) // num_steps // num_processes
+
+    num_updates = 1
+    ave_cos_sims = []
 
     for j in range(num_updates):
 
@@ -135,46 +152,59 @@ def main():
             utils.update_linear_schedule(
                 agent.optimizer, j, num_updates, lr)
 
-        for step in range(num_steps):
-            # Sample actions
+        grad_estimates = []
+        for i in range(10):
+            for step in range(num_steps):
+                # Sample actions
+                with torch.no_grad():
+                    value, action, action_log_prob, recurrent_hidden_states = actor_critic.act(
+                        rollouts.obs[step], rollouts.recurrent_hidden_states[step],
+                        rollouts.masks[step])
+
+                # Obser reward and next obs
+                obs, reward, done, infos = envs.step(action)
+
+                for info in infos:
+                    if 'episode' in info.keys():
+                        episode_rewards.append(info['episode']['r'])
+
+                # If done then clean the history of observations.
+                masks = torch.FloatTensor(
+                    [[0.0] if done_ else [1.0] for done_ in done])
+                bad_masks = torch.FloatTensor(
+                    [[0.0] if 'bad_transition' in info.keys() else [1.0]
+                    for info in infos])
+                rollouts.insert(obs, recurrent_hidden_states, action,
+                                action_log_prob, value, reward, masks, bad_masks)
+
             with torch.no_grad():
-                value, action, action_log_prob, recurrent_hidden_states = actor_critic.act(
-                    rollouts.obs[step], rollouts.recurrent_hidden_states[step],
-                    rollouts.masks[step])
+                next_value = actor_critic.get_value(
+                    rollouts.obs[-1], rollouts.recurrent_hidden_states[-1],
+                    rollouts.masks[-1]).detach()
+                next_value_big = actor_critic.get_value(
+                    rollouts_big.obs[-1], rollouts_big.recurrent_hidden_states[-1],
+                    rollouts_big.masks[-1]
+                ).detach()
 
-            # Obser reward and next obs
-            obs, reward, done, infos = envs.step(action)
+            rollouts.compute_returns(next_value, use_gae, gamma,
+                                    gae_lambda, use_proper_time_limits)
+            
+            grad_est = agent.get_grad_vector(rollouts)
+            grad_estimates.append(grad_est)
 
-            for info in infos:
-                if 'episode' in info.keys():
-                    episode_rewards.append(info['episode']['r'])
+            # cosine_sim = torch.nn.functional.cosine_similarity(grad_full, grad_est, dim=0)
+            # print("Cosine similarity:", cosine_sim.item())
 
-            # If done then clean the history of observations.
-            masks = torch.FloatTensor(
-                [[0.0] if done_ else [1.0] for done_ in done])
-            bad_masks = torch.FloatTensor(
-                [[0.0] if 'bad_transition' in info.keys() else [1.0]
-                 for info in infos])
-            rollouts.insert(obs, recurrent_hidden_states, action,
-                            action_log_prob, value, reward, masks, bad_masks)
-            rollouts_5m.insert(obs, recurrent_hidden_states, action,
-                            action_log_prob, value, reward, masks, bad_masks)
+            if i == 0:
+                value_loss, action_loss, dist_entropy = agent.update(rollouts)
 
-        with torch.no_grad():
-            next_value = actor_critic.get_value(
-                rollouts.obs[-1], rollouts.recurrent_hidden_states[-1],
-                rollouts.masks[-1]).detach()
+            rollouts.after_update()
 
-        rollouts.compute_returns(next_value, use_gae, gamma,
-                                 gae_lambda, use_proper_time_limits)
-
-        value_loss, action_loss, dist_entropy = agent.update(rollouts)
-
-        rollouts.after_update()
-
-        # with open('./store/rollout-10m.pkl', 'rb') as rollout_file:
-        #     rollouts_test = pickle.load(rollout_file)
-
+        cos_sims = []
+        for a, b in itertools.combinations(grad_estimates, 2):
+            cos_sims.append(torch.nn.functional.cosine_similarity(a, b, dim=0))
+        cos_sims = torch.cat(cos_sims)
+        ave_cos_sims.append(cos_sims.mean().item())
 
         # save for every interval-th episode or for the last epoch
         # if (j % save_interval == 0
@@ -208,14 +238,6 @@ def main():
         #     ob_rms = utils.get_vec_normalize(envs).ob_rms
         #     evaluate(actor_critic, ob_rms, args.env_name, args.seed,
         #              args.num_processes, eval_log_dir, device)
-    
-    with open('./store/rollout-100k-flat.pkl', 'wb') as rollout_file:
-        try:
-            os.makedirs('./store')
-        except FileExistsError:
-            pass
-        pickle.dump(rollouts_5m, rollout_file, protocol=4)
-
 
 if __name__ == "__main__":
     main()